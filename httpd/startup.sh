205c82840852ae0019fc86db84d736ea48316048
FumiakiUeda <fumiaki.ueda@topcreation.co.jp>
2024-03-09 22:21:27 +0900

    fix: 環境に依存する部分をenvファイルに分離 upd: httpdとphpを別コンテナに分けイメージを軽量化 del: 不要なファイルを削除

--- httpd/startup.sh
+++ httpd/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 変数定義
-GRPNAME="rkc"
+GRPNAME="xxx"
 
 # 古いシンボリックリンクを削除
 cd /home/${GRPNAME}/
@@ -15,10 +15,11 @@ rm -f batch/news_test/playlist \
       batch/news_support/means
 
 # 新しいシンボリックリンクを作成
-ln -fs /home/${GRPNAME}/ftp_test/vas/playlist /home/${GRPNAME}/batch/news_test/playlist
 ln -fs /home/${GRPNAME}/ftp/vas/playlist      /home/${GRPNAME}/batch/news/playlist
-ln -fs /home/${GRPNAME}/ftp_test/vas/playlist /home/${GRPNAME}/web/news_test/playlist
+ln -fs /home/${GRPNAME}/ftp_test/vas/playlist /home/${GRPNAME}/batch/news_test/playlist
 ln -fs /home/${GRPNAME}/ftp/vas/playlist      /home/${GRPNAME}/web/news/playlist
-# ln -fs ftp/japrs batch/news_support/japrs
-# ln -fs ftp_test/japrs batch/news_test/japrs
-# ln -fs ftp/means batch/news_support/means
+ln -fs /home/${GRPNAME}/ftp_test/vas/playlist /home/${GRPNAME}/web/news_test/playlist
+# ln -fs /home/${GRPNAME}/ftp/japrs /home/${GRPNAME}/batch/news/japrs
+# ln -fs /home/${GRPNAME}/ftp_test/japrs /home/${GRPNAME}/batch/news_test/japrs
+# ln -fs /home/${GRPNAME}/ftp/means /home/${GRPNAME}/batch/news/means
+# ln -fs /home/${GRPNAME}/ftp_test/means /home/${GRPNAME}/batch/news_test/means
